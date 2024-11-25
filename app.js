513e2cb4469ce6adff3a828172bbfae71f7233c3
Pankaj deep Sahota <41828185+pankajdeep@users.noreply.github.com>
2024-11-25 17:38:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ app.listen(PORT, () => {
 });
 
 const db = mysql.createConnection({
-    host: "sql5.freesqldatabase.com",
-    user: "sql5747434",
-    password:"IJNFq1agaX",
-    database:"sql5747434"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 });
 db.connect((err) => {
     if (err) {
